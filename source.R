1967bfb338c19d8167768e9a766543855aad6ee9
Abdullah Oztop <62008763+oztop@users.noreply.github.com>
2020-04-02 23:15:16 +0300

    Update source.R

--- source.R
+++ source.R
@@ -18,35 +18,36 @@ hijri$hijri.months <- case_when(hijri$months==1 ~ "Muharrem",
                                 hijri$months==10 ~ "Şevval",
                                 hijri$months==11 ~ "Zilkade",
                                 hijri$months==12 ~ "Zilhicce",
-)
+                                )
 
 
 
 # assign the leap years and normal years
 
-hijri$leap <- ifelse(hijri$years%%30 == 2|
-                       hijri$years%%30 == 5 |
-                       hijri$years%%30 == 7  |
-                       hijri$years%%30 == 10 |
-                       hijri$years%%30 == 13  |
-                       hijri$years%%30 == 16  |
-                       hijri$years%%30 == 18 | 
-                       hijri$years%%30 == 21 |
-                       hijri$years%%30 == 24 |
-                       hijri$years%%30 == 26 |
-                       hijri$years%%30 == 29, 
+hijri$leap <- hijri$leap <- ifelse(
+                     hijri$years%%30 == 2  |
+                     hijri$years%%30 == 5  |
+                     hijri$years%%30 == 7  |
+                     hijri$years%%30 == 10 |
+                     hijri$years%%30 == 13 |
+                     hijri$years%%30 == 16 |
+                     hijri$years%%30 == 18 | 
+                     hijri$years%%30 == 21 |
+                     hijri$years%%30 == 24 |
+                     hijri$years%%30 == 26 |
+                     hijri$years%%30 == 29, 
                      "Leap", "Normal"
-)
+                      )
 
 ### calibrate the 29s and 30s of normal months (mind the new variable hijri.new)
 
-hijri.new <- hijri[!grepl(2, hijri$months) &
+hijri.new <- hijri[!grepl(2, hijri$months)   &
                      !grepl(4, hijri$months) &
                      !grepl(6, hijri$months) &
                      !grepl(8, hijri$months) &
-                     !grepl(10, hijri$months) |
-                     
-                     !grepl(30, hijri$days) | !grepl("Normal", hijri$leap) ,]    
+                     !grepl(10, hijri$months)|
+                     !grepl(30, hijri$days)  | 
+                     !grepl("Normal", hijri$leap) ,]    
 
 
 # calibrate the 29s and 30s of leap years (mind the new variable hijri.new1)
@@ -69,7 +70,7 @@ hijri.new1$weekdays <- case_when(
   hijri.new1$days%%7 == 2 ~ "Friday",
   hijri.new1$days%%7 == 3 ~ "Saturday",
   hijri.new1$days%%7 == 4 ~ "Sunday"
-) 
+                                ) 
 
 
 
